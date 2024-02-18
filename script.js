cd566c4c84b876d0c2bd8e561f069b29dff928b5
enghirushan <102861747+enghirushan@users.noreply.github.com>
2024-02-18 06:03:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("b1").onclick = function(){
+
+  document.getElementById("p1").style.color = "red";
+  
+}
+document.getElementById("b2").onclick = function(){
+
+  document.getElementById("p1").style.fontSize = "20px";
+  
+}
+document.getElementById("b3").onclick = function(){
+
+  document.getElementById("p1").style.display = "none";
+  
+}
